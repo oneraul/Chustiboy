8d3971e25b1ae24b29e4c99361f3b4cfee19d344
oneraul <oneraul@gmail.com>
2015-12-30 02:00:37 +0100

    AnimatedSprite mejorada un poco

--- Chustillas/src/chustiboy/gameplay/AnimatedSprite.java
+++ Chustillas/src/chustiboy/gameplay/AnimatedSprite.java
@@ -4,15 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class AnimatedSprite {
 	private TextureRegion region;
 	private AnimationFrame[] frames;
 	private int current;
 	private float acumulator;
+	private Vector2 tmpOrigin;
 	
 	public AnimatedSprite(Texture texture) {
 		region = new TextureRegion(texture);
+		tmpOrigin = new Vector2();
 	}
 	
 	public void animate() {
@@ -32,13 +35,23 @@ public class AnimatedSprite {
 	}
 	
 	public void draw(SpriteBatch batch, float x, float y, float scale) {
-		batch.draw(region, x-frames[current].originX, y-frames[current].originY, region.getRegionWidth()*scale, region.getRegionHeight()*scale);
+		batch.draw(
+			region, 							// TextureRegion
+			x-frames[current].originX * scale,	// x
+			y-frames[current].originY * scale,	// y
+			frames[current].width	  * scale,	// width
+			frames[current].height	  * scale	// height
+		);
 	}
 	
 	public void setFrames(AnimationFrame[] frames) {
-		acumulator = 0;
+		acumulator  = 0;
+		current     = 0;
 		this.frames = frames;
-		current = 0;
 		region.setRegion(frames[current].x, frames[current].y, frames[current].width, frames[current].height);
+	}
+	
+	public Vector2 getOrigin() {
+		return tmpOrigin.set(frames[current].originX, frames[current].originY);
 	}
 }
